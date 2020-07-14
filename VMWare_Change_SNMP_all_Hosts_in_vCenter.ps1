a00b57fe054657a807091c556c2fdbec1982748d
Sergio Alegre <sergio.alegre.arribas@gmail.com>
2020-07-14 12:16:07 +0200

    Create VMWare_Change_SNMP_all_Hosts_in_vCenter.ps1

---
+++ VMWare_Change_SNMP_all_Hosts_in_vCenter.ps1
@@ -0,0 +1,33 @@
+# conectar vcenter
+cls
+Add-PSSnapin VMware.VimAutomation.Core
+$user = "PUT_USERNAME_HERE"
+$pass = "PUT_PASSWORD_HERE"
+$vcenter = "PUT_VCENTER_HERE"
+Connect-VIServer -Server $vcenter -User $user -Password $pass | Out-Null
+
+
+# conesguir lista de hosts
+$lista = Get-VMHost
+
+
+# bucle para planchar la config snmp a todos
+ForEach ($VMHost in $lista)
+   { 
+    $esxcli = Get-VMHost $VMHost | Get-Esxcli
+    $esxcli.system.snmp.set($null,'public',$true,$null,$null,$null,$null,$null,'161',$null,$null,$null,$null,$null,'10.30.240.40@162/public',$null,$null)
+   }
+
+
+# ejemplo escribir en 1 host concreto
+#$esxcli =  Get-VMHost ryasvm03.grupoantolin.com | Get-Esxcli
+#$esxcli.system.snmp.set($null,'public',$true,$null,$null,$null,$null,$null,'161',$null,$null,$null,$null,$null,'10.30.240.40@162/public',$null,$null)
+
+
+# comprobar un host en concreto
+#$esxcli =  Get-VMHost ryasvm03.grupoantolin.com | Get-Esxcli
+#$esxcli.system.snmp.get()
+
+
+# permitir trafico en firewall
+#$esxcli.network.firewall.ruleset.set –-ruleset-id snmp -–allowed-all true
