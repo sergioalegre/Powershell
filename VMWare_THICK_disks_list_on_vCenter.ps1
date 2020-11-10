b9109a08e7e22974a41d8308c5a1c700d1e4b17b
Sergio Alegre <sergio.alegre.arribas@gmail.com>
2020-11-10 09:18:14 +0100

    Update VMWare_THICK_disks_list_on_vCenter.ps1

--- VMWare_THICK_disks_list_on_vCenter.ps1
+++ VMWare_THICK_disks_list_on_vCenter.ps1
@@ -1,3 +1,19 @@
-#listaremos todas las VMs con algun disco THICK, excepto las que es normal que lo tengan
+#listaremos todas las VMs con algun disco THICK, excepto las que es normal que lo tengan, en orden descendiente de tamaño
 
-Get-VM | Get-HardDisk | Where {$_.storageformat -eq "Thick" -and $_.Parent -NotLike "*VARIV*" -and $_.Parent -notlike "*VMRIV*" -and $_.Parent -notlike "*_MigratedITIBlock*"} | Select Parent, Name, CapacityGB, storageformat | FT -AutoSize
+cls
+
+
+Get-VM | Get-HardDisk | Where {$_.storageformat -eq "Thick" `
+-and $_.Name -NotLike "*VARIV*" `
+-and $_.Name -NotLike "*VMCM*" `
+-and $_.Name -NotLike "*VMRIV*" `
+-and $_.Name -NotLike "*_MigratedITIBlock*" `
+-and $_.Name -NotLike "*CORE*" `
+-and $_.Name -NotLike "*WLC*" `
+-and $_.Name -NotLike "*_replica" `
+-and $_.Name -NotLike "*_lyvesync" `
+-and $_.Name -NotLike "*VANAC*"  `
+-and $_.Name -NotLike "*VMCOM0*" `
+-and $_.Name -NotLike "*VMCM0*" `
+-and $_.Name -NotLike "*VMDC0*" `
+-and $_.Name -NotLike "GANVMSAP0*"} | Select Parent, Name, CapacityGB, storageformat | Sort -Property CapacityGB -Descending
