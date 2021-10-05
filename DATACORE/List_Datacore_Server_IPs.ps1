a5c7390bdaf91bffe5f53cadc637973f6181334a
SergioAlegre <sergio.alegre.arribas@gmail.com>
2021-10-05 17:14:48 +0200

    Create List_Datacore_Server_IPs.ps1

---
+++ DATACORE/List_Datacore_Server_IPs.ps1
@@ -0,0 +1,17 @@
+﻿#variables
+$vcenter ="vcenter_server_name"
+
+cls
+
+Import-module vmware.vimautomation.core
+Connect-VIServer -Server $vcenter
+$datacore_servers= Get-VM | Where {$_.Name -Like "*CORE0*"}
+
+foreach ($dcs in $datacore_servers){
+    $IPs=$dcs.Guest.IPAddress
+    foreach ($ip in $IPs){
+        if($ip -notlike "192.168.*" -and $ip -notlike "fe80*"){
+            Write-Host $dcs.Name $ip
+        }
+    }
+}
