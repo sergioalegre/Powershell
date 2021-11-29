b556e1b658e5f3b2c6e57eab8c08db6c1dcf3428
SergioAlegre <sergio.alegre.arribas@gmail.com>
2021-11-29 12:31:52 +0100

    Update WINDOWS_ejecutar_comando_en_server_remoto.ps1

--- Windows/WINDOWS_ejecutar_comando_en_server_remoto.ps1
+++ Windows/WINDOWS_ejecutar_comando_en_server_remoto.ps1
@@ -1,12 +1,32 @@
 #Nota: solo funciona si hay comunicacion no bloqueada por firewall
 
-#Highlights: invocar comando remoto
+#Highlights: invocar comando remoto mediante sesión
+
+#variables
+$vCenter = <ip_vcenter>
+$server_monitorizacion= <ip_server_monitorizacion>
 
 cls
-$localhost = $dato = Invoke-Command -ScriptBlock {hostname}
-Write-Host "Estamos en" $localhost
-$remote_host="<remote_host_here>"
-$ses = New-PSSession -ComputerName $remote_host
-$dato = Invoke-Command -Session $ses -ScriptBlock {hostname}
-Remove-PSSession $ses
-Write-Host $vm tiene de hostname $dato
+Import-module vmware.vimautomation.core
+Connect-VIServer -Server $vCenter
+cls
+
+$lista= Get-VM | Where { $_.Name -Like "*VMXXX0*"  } | Sort -Property Name
+Write-Host "How many XXX servers we have and where: " $lista.Count
+foreach ($vm in $lista){
+    if(Test-Connection -Computername $vm -BufferSize 16 -Count 1 -Quiet)
+    {
+        $sl=(((get-view -viewtype VirtualMachine -filter @{"Name"=$vm.Name}).config).Annotation).split()[-1]
+        $ses = New-PSSession -ComputerName $vm            
+        $contador=@();
+        #netstat -an | findstr $server_monitorizacion | find /c /v ""
+        $contador = Invoke-Command -Session $ses -ScriptBlock {netstat -an | findstr $server_monitorizacion}
+        Remove-PSSession $ses
+        if($contador.Count -gt 100){
+            Write-Host $vm  es $sl y tiene $contador.Count conexiones a Solarwinds -ForegroundColor Red
+        }
+        else{
+            Write-Host $vm  es $sl y tiene $contador.Count conexiones a Solarwinds -ForegroundColor Green
+        }
+    }
+}
